a6eeec5000b18e7273e54a84d62a4b6dbdca69b8
antosa2972 <55Ssteam29>
2021-05-20 18:50:54 +0300

    Implemented ArrayListProductDaoTest using junit4.

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,10 +3,13 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -16,6 +19,50 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testSaveNewProduct() throws ProductNotFoundException {
+        String product_code = "product-test";
+        Product product = new Product(product_code, "Siemens C56", new BigDecimal(70), Currency.getInstance("USD"), 20, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20C56.jpg");
+        productDao.save(product);
+        assertTrue(product.getId() > 0);
+        Product result = productDao.getProduct(Long.valueOf(product.getId()));
+        assertNotNull(result);
+        assertEquals(product_code, result.getCode());
+    }
+
+    @Test
+    public void findProductWithZeroStock() {
+        List<Product> productList = productDao.findProducts();
+        for (Product prod :
+                productList) {
+            assertFalse(prod.getStock() < 0);
+        }
+    }
+
+    @Test
+    public void findNullPriceProduct() {
+        List<Product> productList = productDao.findProducts();
+        for (Product prod :
+                productList) {
+            assertNotNull(prod.getPrice());
+        }
+    }
+
+    @Test(expected = ProductNotFoundException.class)
+    public void deleteProductTest() throws ProductNotFoundException {
+        Long id = 2L;
+        Product product = productDao.getProduct(id);
+        assertEquals(product.getId(), id);
+        productDao.delete(id);
+        product = productDao.getProduct(id);
+    }
+
+    @Test
+    public void inStockTest() throws ProductNotFoundException {
+        Product product = productDao.getProduct(3L);
+        assertTrue(product.getStock() > 0);
     }
 }
