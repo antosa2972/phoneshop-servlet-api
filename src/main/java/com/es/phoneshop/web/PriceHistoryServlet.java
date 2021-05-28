090a1a336b94a1b9fa1f3861a5299d19c37c4510
antosa2972 <55Ssteam29>
2021-05-28 23:18:40 +0300

    Add PriceHistoryServlet

---
+++ src/main/java/com/es/phoneshop/web/PriceHistoryServlet.java
@@ -0,0 +1,27 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.ProductDao;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PriceHistoryServlet extends HttpServlet {
+    private ProductDao productDao;
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+        productDao = ArrayListProductDao.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getPathInfo();
+        request.setAttribute("product", productDao.getProduct(Long.valueOf(id.substring(1))));
+        request.getRequestDispatcher("/WEB-INF/pages/price_history.jsp").forward(request, response);
+    }
+}
