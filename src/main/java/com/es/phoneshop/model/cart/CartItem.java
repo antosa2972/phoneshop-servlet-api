5ce4f5822947addd6b26c1dc9090bfde888b147c
antosa2972 <55Ssteam29>
2021-06-12 01:48:41 +0300

    Add getters and setters for new functionality

--- src/main/java/com/es/phoneshop/model/cart/CartItem.java
+++ src/main/java/com/es/phoneshop/model/cart/CartItem.java
@@ -2,7 +2,9 @@ package com.es.phoneshop.model.cart;
 
 import com.es.phoneshop.model.product.Product;
 
-public class CartItem {
+import java.io.Serializable;
+
+public class CartItem implements Serializable {
     private Product product;
     private int quantity;
 
@@ -17,6 +19,10 @@ public class CartItem {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
     @Override
